b7709615b6ae21e626f3712a5dbde3046f0b9487
geesaladurgaakhil <56544604+geesaladurgaakhil@users.noreply.github.com>
2024-12-04 06:40:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,70 @@
+# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "3.114.0"
     }
   }
+  
+  required_version = ">= 1.0.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = var.resource_group_name_prefix
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-resource "azurerm_storage_account" "staccount" {
-    name = var.storage_account_name
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    account_tier = "Standard"
-    account_replication_type = "GRS"
+resource "azurerm_storage_account" "example" {
+  name                = "testarcsub65jdrf"
+  resource_group_name = azurerm_resource_group.example.name
 
-    tags = {
-        environment = "testigakki"
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  blob_properties {
+    change_feed_enabled           = "true"
+    change_feed_retention_in_days = 7
+    versioning_enabled            = "true"
+    delete_retention_policy {
+      days = 7
     }
+    restore_policy {
+      days = 6 # Retain point-in-time restore capability for 6 days
+    }
+    container_delete_retention_policy {
+      days = "14"
+    }
+  }
+  tags = {
+    environment = "staging"
+  }
 }
 
+resource "azurerm_storage_management_policy" "example" {
+  storage_account_id = azurerm_storage_account.example.id
+  rule {
+    name    = "versiondelete"
+    enabled = true
+    filters {
+      prefix_match = ["activedata/"]
+      blob_types   = ["blockBlob"]
+    }
+    actions {
+      version {
+        delete_after_days_since_creation = 7
+      }
+    }
+  }
+}
+
+resource "azurerm_storage_container" "statefile" {
+  name                  = "statefiles"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
